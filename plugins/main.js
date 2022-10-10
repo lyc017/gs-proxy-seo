35dc559989a2c71cbdabf28ff986dfa73f7a973a
lyc017 <2728500190@qq.com>
2022-10-10 18:41:09 +0800

    重构

--- plugins/main.js
+++ plugins/main.js
@@ -1,10 +1,12 @@
 // 主入口----设置不需要在服务端渲染的内容
 import Vue from 'vue'
+
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 import util from '@/static/js/util/index.js'
 import * as Base64 from 'js-base64'
 import animate from '../node_modules/animate.css/animate.css'
+
 
 Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 Vue.prototype.$util = util
